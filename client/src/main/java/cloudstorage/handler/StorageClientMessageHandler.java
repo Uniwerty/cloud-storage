01ac9599b04e3bab20a493129738583bcfd7bf07
Uniwerty <ivcdim@yandex.ru>
2023-10-21 22:01:35 +0300

    Merge ClientMessageHandler and StorageClientMessageHandler

--- client/src/main/java/cloudstorage/handler/StorageClientMessageHandler.java
+++ client/src/main/java/cloudstorage/handler/StorageClientMessageHandler.java
@@ -1,17 +1,25 @@
 package cloudstorage.handler;
 
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class StorageClientMessageHandler extends ClientMessageHandler {
-    /**
-     * Handles a message {@code String} read from {@code Channel}.
-     *
-     * @param ctx the {@link ChannelHandlerContext} of the current {@code ChannelHandler}
-     * @param msg the message {@code String} to handle
-     * @throws Exception if an error occurred during handling
-     */
+public class StorageClientMessageHandler extends SimpleChannelInboundHandler<String> {
+    protected static final Logger logger = LoggerFactory.getLogger(StorageClientMessageHandler.class);
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
         logger.info("Server response: {}", msg);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        logger.info("Connected to the server");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.error("Exception occurred: {}", cause.getMessage());
     }
 }
